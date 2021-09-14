f1c90ecdc21c4bdda964895acbc123405f4c5d74
Sean <thesean.kim33@gmail.com>
2021-09-13 23:56:10 -0500

    Uploaded first draft of finished criteria

---
+++ Assets/EightBallScriptCube2.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class EightBallScriptCube2 : MonoBehaviour
+{
+
+    public GameObject Magic8TextObject;
+    public GameObject KnickKnackObject;
+
+    private AudioSource source;
+
+    public bool hasBeenFlipped;
+
+    public string[] eightBallQuotes = new string[] { "What did you just say?", "That's epic", "👍", "👎", "uuhhhh", "The money is calling", "Something bad is going to happen", "The future looks grim", "Sometimes I get it I don't even want it", "idk man", "it's a hard no" };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        source = GetComponent<AudioSource>();
+        Magic8TextObject.GetComponent<TextMeshPro>().text = "Flip Me";
+        hasBeenFlipped = false;
+        InvokeRepeating("keepTrackOfPosition", 1f, 1f);
+
+    }
+
+    void keepTrackOfPosition()
+    {
+        var xPos = UnityEditor.TransformUtils.GetInspectorRotation(KnickKnackObject.transform).x;
+        //Debug.Log(":\nReceived: " + xPos);
+
+        /* If Rotation of X is between -20 to 0 or 0 to 20, then it is sitting */
+
+        /* If Rotation of X is between 170 to 180 or -180 to -170 */
+
+        if ((xPos < 20 && xPos > 0) || (xPos > -20 && xPos < 0))
+        {
+            /* Cube is in sitting position */
+            if (hasBeenFlipped == true)
+            {
+                /* Choose a random classic saying */
+                int index = Random.Range(0, eightBallQuotes.Length);
+
+                /* Play a sound effect */
+                source.Play();
+                /* Set hasBeenFlipped to false */
+                hasBeenFlipped = false;
+
+                /* Debug Statement */
+                //Debug.Log(":\nQuote Position: " + index);
+                Magic8TextObject.GetComponent<TextMeshPro>().text = eightBallQuotes[index];
+            }
+        }
+
+        if ((xPos < 180 && xPos > 170) || (xPos > -180 && xPos < -170))
+        {
+            /* Cube is upside down */
+            hasBeenFlipped = true;
+
+            /* Debug Statement */
+            //Magic8TextObject.GetComponent<TextMeshPro>().text = "Flipped";
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/WeatherAPICube2.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+using TMPro;
+
+public class WeatherAPICube2 : MonoBehaviour
+{
+
+    public GameObject weatherTextObject;
+    // add your personal API key after APPID= and before &units= 
+    string url = "http://api.openweathermap.org/data/2.5/weather?lat=37.76653935415943&lon=-122.48634762042931&APPID=10f0f2739199bdfca8d826a6307db86f&units=metric";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("GetDataFromWeb", 2f, 600f);
+    }
+    void GetDataFromWeb()
+    {
+
+        StartCoroutine(GetRequest(url));
+    }
+
+    IEnumerator GetRequest(string uri)
+    {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
+        {
+            // Request and wait for the desired page.
+            yield return webRequest.SendWebRequest();
+
+
+            if (webRequest.isNetworkError)
+            {
+                Debug.Log(": Error: " + webRequest.error);
+            }
+            else
+            {
+                // print out the weather data to make sure it makes sense
+                Debug.Log(":\nReceived: " + webRequest.downloadHandler.text);
+
+
+                // grab the current temperature and simplify it if needed
+                int startTemp = webRequest.downloadHandler.text.IndexOf("temp", 0);
+                int endTemp = webRequest.downloadHandler.text.IndexOf(",", startTemp);
+                double tempF = float.Parse(webRequest.downloadHandler.text.Substring(startTemp + 6, (endTemp - startTemp - 6)));
+                int easyTempF = Mathf.RoundToInt((float)tempF);
+                //Debug.Log ("integer temperature is " + easyTempF.ToString());
+                int startConditions = webRequest.downloadHandler.text.IndexOf("main", 0);
+                int endConditions = webRequest.downloadHandler.text.IndexOf(",", startConditions);
+                string conditions = webRequest.downloadHandler.text.Substring(startConditions + 7, (endConditions - startConditions - 8));
+                //Debug.Log(conditions);
+
+                weatherTextObject.GetComponent<TextMeshPro>().text = "" + easyTempF.ToString() + "°C\n" + conditions;
+            }
+        }
+    }
+}
